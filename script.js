d61e87e87deea9b1274ec1a123cd774f9f025417
Bruno Badac <60857482+brunobadac@users.noreply.github.com>
2026-04-15 12:02:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const list = document.getElementById("taskList");
 
+  if (input.value === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = input.value;
+
+  const btn = document.createElement("button");
+  btn.textContent = "X";
+
+  btn.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(btn);
+  list.appendChild(li);
+
+  input.value = "";
+}
